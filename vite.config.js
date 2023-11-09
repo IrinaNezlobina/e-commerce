701955f4492f041d31d4a5230551d12e0df250c6
IrinaNezlobina <85892976+IrinaNezlobina@users.noreply.github.com>
2023-11-09 22:00:15 +0300

    fix )

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    extensions: ['.js', '.vue', '.json', '.scss'],
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      // styles: resolve('src/assets/scss')
+    plugins: [
+        vue(),
+    ],
+    resolve: {
+        extensions: ['.js', '.vue', '.json', '.scss'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            // styles: resolve('src/assets/scss')
+        }
     }
-  }
-}
+})
