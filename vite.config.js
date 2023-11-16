874e3ed5d2086ef44e5d2c2908ddf7ddec2f1380
IrinaNezlobina <85892976+IrinaNezlobina@users.noreply.github.com>
2023-11-16 20:15:30 +0300

    delpoy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     // base: env.NODE_ENV === 'production' ? 'prod' : 'dev',
-    base: 'https://irinanezlobina.github.io/e-commerce/',
+    base: '/e-commerce/',
     plugins: [
         vue(),
     ],
